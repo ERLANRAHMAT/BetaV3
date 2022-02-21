59e4898302db107ac4602b4f1a5e2c104dd29621
ERLANRAHMAT <erlanrahmatdwisaputra@gmail.com>
2022-02-21 19:16:23 +0700

    BetaV3

---
+++ ryudata/welkam.js
@@ -0,0 +1,21 @@
+//EDIT SESUKA KALIAN 😉
+
+// UCAPAN WELCOME
+exports.welkam = () => {
+	return `Intro Dulu Yuk Biar Keren 🤙
+
+📛 Nama : 
+🔞 Umur :
+🏙️ Askot :
+
+Semoga Kamu Senang Berada Disini
+Serta Jangan Lupa Untuk Membaca
+Dan Mematuhi Rules Yang Ada
+`
+}
+
+// UCAPAN GOODBYE
+exports.goodbye = () => {
+	return `GoodByee Kakaa Selamat Tinggal Semoga Balik Lagi🤗
+`
+}
