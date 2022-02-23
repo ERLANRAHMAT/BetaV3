f43289c82211257df4c745ef2ebed2cc8b50214e
Tokusat <98427364+ERLANRAHMAT@users.noreply.github.com>
2022-02-23 16:00:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const setting = JSON.parse(fs.readFileSync('./setting.json'))
     
     //Status Bot
     global.autoread = false // auto read pesan / message
-    global.autorecording = true //status auto merekam ( auto record )
+    global.autorecording = false //status auto merekam ( auto record )
     global.autoketik = false //status auto mengetik (auto typing)
     global.available = false //status online (online)
     
